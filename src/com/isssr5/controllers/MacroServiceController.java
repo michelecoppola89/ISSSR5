6a89778b68c299cb8d97d2a22065276d7414ae37
michele <michelecoppola89@gmail.com>
2014-04-26 19:35:02 +0200

    sprint 5 without tests

--- src/com/isssr5/controllers/MacroServiceController.java
+++ src/com/isssr5/controllers/MacroServiceController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import org.apache.commons.math3.exception.OutOfRangeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.isssr5.entities.ServiceUser;
 import com.isssr5.exceptions.NotExistingOperandException;
 import com.isssr5.exceptions.NotExistingUserException;
 import com.isssr5.exceptions.WrongDistributionException;
+import com.isssr5.exceptions.WrongScaleForMacroServiceId;
 import com.isssr5.service.MacroServiceTransaction;
 import com.isssr5.service.ServiceUserTransaction;
 
@@ -30,7 +32,7 @@ public class MacroServiceController {
 			ServiceUserTransaction serviceUserTransaction) {
 		this.macroServiceTransaction = macroServiceTransaction;
 		this.serviceUserTransaction = serviceUserTransaction;
-		
+
 	}
 
 	private MacroServiceTransaction macroServiceTransaction;
@@ -43,10 +45,12 @@ public class MacroServiceController {
 	public @ResponseBody
 	Result ResolveMacroServiceUrl(@PathVariable String userId,
 			@PathVariable String msId, @PathVariable String parameters)
-			throws NotExistingUserException, NumberFormatException, NotExistingOperandException, WrongDistributionException {
-		List <ResultValue> resultVList= new ArrayList<ResultValue>();
+			throws NotExistingUserException, NumberFormatException,
+			NotExistingOperandException, WrongDistributionException,
+			OutOfRangeException, WrongScaleForMacroServiceId {
+		List<ResultValue> resultVList = new ArrayList<ResultValue>();
 		ResolveMacroService(userId, msId, parameters, resultVList);
-		Result result=new Result();
+		Result result = new Result();
 		result.setResultValueList(resultVList);
 		return result;
 
@@ -54,7 +58,9 @@ public class MacroServiceController {
 
 	public void ResolveMacroService(String user, String msId,
 			String parameters, List<ResultValue> results)
-			throws NotExistingUserException, NumberFormatException, NotExistingOperandException, WrongDistributionException {
+			throws NotExistingUserException, NumberFormatException,
+			NotExistingOperandException, WrongDistributionException,
+			OutOfRangeException, WrongScaleForMacroServiceId {
 		// List<Long> idOperands = new ArrayList<Long>();
 		StringTokenizer st = new StringTokenizer(parameters, ",");
 		List<String> parametersArray = new ArrayList<String>();
@@ -105,14 +111,13 @@ public class MacroServiceController {
 
 		}
 
-	
-
 	}
 
 	public Result ResolveElementaryService(String user, String service,
 			List<String> parametersArray) throws NumberFormatException,
 			NotExistingUserException, NotExistingOperandException,
-			WrongDistributionException {
+			WrongDistributionException, OutOfRangeException,
+			WrongScaleForMacroServiceId {
 
 		if (service.equals("ConfidenceInterval"))
 			return Kolmogorov_ConfidenceIntervalController
@@ -129,15 +134,23 @@ public class MacroServiceController {
 					.getTwoSampleKolmogrov(user,
 							Long.parseLong(parametersArray.get(0)),
 							Long.parseLong(parametersArray.get(0)));
+
 		else if (service.equals("TTEST_STAT"))
-			return ParametricTestController.getTTestAlphaTwoSided(user,
+			return ParametricTestController.getTTestStat(user,
 					Long.parseLong(parametersArray.get(0)),
-					Double.parseDouble(parametersArray.get(1)),
-					Double.parseDouble(parametersArray.get(2)));
+					Double.parseDouble(parametersArray.get(1)));
+
 		else if (service.equals("TTEST_PVALUE_2SIDED"))
 			return ParametricTestController.getTTestPValueTwoSided(user,
 					Long.parseLong(parametersArray.get(0)),
 					Double.parseDouble(parametersArray.get(1)));
+
+		else if (service.equals("TTEST_ALPHA_2SIDED"))
+			return ParametricTestController.getTTestAlphaTwoSided(user,
+					Long.parseLong(parametersArray.get(0)),
+					Double.parseDouble(parametersArray.get(1)),
+					Double.parseDouble(parametersArray.get(2)));
+
 		else if (service.equals("TTEST_1SIDED_LESSEQUAL"))
 			return ParametricTestController.getTTestOneSidedLessEqual(user,
 					Long.parseLong(parametersArray.get(0)),
@@ -165,7 +178,7 @@ public class MacroServiceController {
 		else if (service.equals("TTEST_STAT2"))
 			return ParametricTestController.getTTestStat(user,
 					Long.parseLong(parametersArray.get(0)),
-					Double.parseDouble(parametersArray.get(1)));
+					Long.parseLong(parametersArray.get(1)));
 
 		else if (service.equals("TTEST_PVALUE_2SIDED_2"))
 			return ParametricTestController.getTTestPValueTwoSided(user,
@@ -212,22 +225,21 @@ public class MacroServiceController {
 			return ParametricTestController.getOneWayAnovaPValue(user,
 					Long.parseLong(parametersArray.get(0)),
 					Long.parseLong(parametersArray.get(1)),
-					Long.parseLong(parametersArray.get(2)),
-					Long.parseLong(parametersArray.get(3)));
+					Long.parseLong(parametersArray.get(2)));
 
 		else if (service.equals("ONEWAY_ANOVA_ALPHA_3OP"))
 			return ParametricTestController.getOneWayAnovaAlpha(user,
 					Long.parseLong(parametersArray.get(0)),
 					Long.parseLong(parametersArray.get(1)),
 					Long.parseLong(parametersArray.get(2)),
-					Long.parseLong(parametersArray.get(3)),
-					Double.parseDouble(parametersArray.get(4)));
+					Double.parseDouble(parametersArray.get(3)));
 
 		else if (service.equals("ONEWAY_ANOVA_PVALUE_4OP"))
 			return ParametricTestController.getOneWayAnovaPValue(user,
 					Long.parseLong(parametersArray.get(0)),
 					Long.parseLong(parametersArray.get(1)),
-					Long.parseLong(parametersArray.get(2)));
+					Long.parseLong(parametersArray.get(2)),
+					Long.parseLong(parametersArray.get(3)));
 		// ULTIME TRE HANNO DUE METODI UGUALI
 
 		else if (service.equals("ONEWAY_ANOVA_ALPHA_4OP"))
@@ -238,83 +250,38 @@ public class MacroServiceController {
 					Long.parseLong(parametersArray.get(3)),
 					Double.parseDouble(parametersArray.get(4)));
 
+		else if (service.equals("mean"))
+			return DescriptiveStatisticController.get_mean(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("variance"))
+			return DescriptiveStatisticController.get_variance(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("min"))
+			return DescriptiveStatisticController.get_minValue(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("max"))
+			return DescriptiveStatisticController.get_maxValue(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("stdDeviation"))
+			return DescriptiveStatisticController.get_standardDeviation(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("median"))
+			return DescriptiveStatisticController.get_median(user,
+					Integer.parseInt(parametersArray.get(0)));
+
+		else if (service.equals("geometricMean"))
+			return DescriptiveStatisticController.get_geometricMean(user,
+					Integer.parseInt(parametersArray.get(0)));
+
 		else
 			return null;
 
 	}
 
-	// private void checkForMacroServiceAcquisition(MacroService ms)
-	// throws NullOperandException, NullOperandTypeException,
-	// NullOperandModeException {
-	// if ((ms.getOperandList() == null) || (ms.getOperandList().size() == 0)) {
-	//
-	// throw new NullOperandException();
-	// }
-	//
-	// for (int i = 0; i < ms.getOperandList().size(); i++) {
-	// if ((ms.getOperandList().get(i).getDataType() == null)
-	// || (!(ms.getOperandList().get(i).getDataType()
-	// .equals("String")) && !(ms.getOperandList().get(i)
-	// .getDataType().equals("Double")))) {
-	// throw new NullOperandTypeException();
-	//
-	// }
-	//
-	// if ((!(ms.getOperandList().get(i).getOperandMode().equals("F"))
-	// && !(ms.getOperandList().get(i).getOperandMode()
-	// .equals("D")) && !(ms.getOperandList().get(i)
-	// .getOperandMode().equals("E")))) {
-	// throw new NullOperandModeException();
-	//
-	// }
-	//
-	// }
-
-	// if((ms.getElementaryServices()==null)||(ms.getElementaryServices().size()==0)){
-	// throw new NullElementaryServiceListException();
-	//
-	// }
-
-	// }
-
-	// @RequestMapping(value = "/testMacroService", method = RequestMethod.GET)
-	// public @ResponseBody
-	// MacroService testMacroservice() {
-	// String idCode = "MEAN";
-	// Operand op1 = new Operand("Double", null, "E");
-	// ArrayList<MacroService> al = new ArrayList<MacroService>();
-	// ArrayList<Operand> opList = new ArrayList<Operand>();
-	// opList.add(op1);
-	// MacroService ms = new MacroService(idCode, opList, al);
-	// ms.setNumOperand(1);
-	// return ms;
-	//
-	// }
-
-	// @RequestMapping(value = "/createMacroService", method =
-	// RequestMethod.POST)
-	// public @ResponseBody
-	// String createMacroservice(@RequestBody MacroService ms)
-	// throws NullOperandException, NullOperandTypeException,
-	// NullOperandModeException, NullElementaryServiceListException,
-	// NotExistingMacroServiceException {
-	//
-	// checkForMacroServiceAcquisition(ms);
-	//
-	// ms.decodeMacroService(dataTable);
-	// String st = "";
-	// if (ms.getElementaryServices() != null) {
-	// // st += "MacroService ID: " + ms.getIdCode() +
-	// // "\nElementary Service: "
-	// // + ms.printElementaryService() + "\nOperandList:\n"
-	// // + ms.printOperandList();
-	// st += ms.printMacroService();
-	// st += "\nOperandList:\n" + ms.printOperandList();
-	// } else
-	// st += "MacroService ID: " + ms.getIdCode() + "\nOperandList:\n"
-	// + ms.printOperandList();
-	//
-	// return st;
-	// }
 
 }
